23d3077a4e48a0b1633e6d74d824c5274ba1b93f
alvarezmarlen <marlenjosefalvarez@gmail.com>
2026-03-10 12:21:34 +0100

    codigos para ser vistos en los archivos

--- MarlenPY/parametros_argumentos/por_defecto.py
+++ MarlenPY/parametros_argumentos/por_defecto.py
@@ -1,14 +1,11 @@
+""" El Argumento es Opcional porque el Parámetro ya tiene un valor por Defecto. """
 # El parámetro 'part2' tiene un valor por defecto
 def nombre_curso(part1, part2="F5"):
     print(f"{part1} {part2}")
     
-nombre_curso("Peñascal") 
+nombre_curso("Peñascal")    # Si la llamada está vacía  =>  Manda el Parámetro ("F5").
 
 
-
-# Caso B: Sí envía el argumento (Sobrescribe)
+# Sí la llamada tiene datos => Manda el Argumento ("Python").
 nombre_curso("Peñascal", "Python") 
 
-""" "El Argumento es Opcional porque el Parámetro ya tiene un valor por Defecto."
-Si la llamada está vacía     Manda el Parámetro ("F5").
-Si la llamada tiene datos    Manda el Argumento ("Python"). """
