626483b6e4bb8f8f9a5e91597782a3ec9196e460
alvarezmarlen <marlenjosefalvarez@gmail.com>
2026-03-12 02:15:37 +0100

    Elimine codigo de canva

--- MarlenPY/retorno_de_valores/retorno_de_colecciones.py
+++ MarlenPY/retorno_de_valores/retorno_de_colecciones.py
@@ -5,32 +5,3 @@ def obtener_usuarios():
 lista = obtener_usuarios() # Resultado: ['Ana', 'Luis', 'Marta']
 print(lista)
 
-# 1. VALOR SIMPLE (Un dato)
-def calcular_iva(precio):
-    return precio * 0.21
-  
-total = calcular_iva(100)  
-print(total)
-
-# 2. MÚLTIPLES VALORES (Empaquetado en Tupla)
-def coordenadas_bilbao():
-    return 43.26, -2.93  # Python crea la tupla automáticamente
-
-resultado = coordenadas_bilbao()
-print(resultado)
-
-
-# 3. COLECCIÓN (Datos agrupados)
-def sedes_f5():
-    return ["Peñascal", "Bolueta", "Madrid"]  # Devuelve una LISTA []
-  
-lista_sedes = sedes_f5()
-print(lista_sedes)
-
-# 4. IMPLÍCITO (Retorno de seguridad)
-def mostrar_aviso():
-    print("Guardado correctamente") 
-
-# Al no haber 'return', devuelve None
-resultado_mostrar = mostrar_aviso()
-print(resultado_mostrar)
