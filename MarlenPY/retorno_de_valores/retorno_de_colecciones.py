8051731521233c3da2f3740c05dffac1c2a191a0
alvarezmarlen <marlenjosefalvarez@gmail.com>
2026-03-13 01:03:05 +0100

    modifique codigo de un ejemplo

--- MarlenPY/retorno_de_valores/retorno_de_colecciones.py
+++ MarlenPY/retorno_de_valores/retorno_de_colecciones.py
@@ -1,7 +1,11 @@
-# 3. Retorno de Colección: Devuelve estructuras (Listas, Dicts, Conjuntos)
-def obtener_usuarios():
-  return ["Ana", "Luis", "Marta"]
+# 3. Definimos la función que devuelve una Colección (una Lista [])
+def obtener_sedes():
+    lista_sedes = ["Peñascal", "Bolueta", "Madrid"]    # Creamos una lista con varios elementos
+    return lista_sedes
 
-lista = obtener_usuarios() # Resultado: ['Ana', 'Luis', 'Marta']
-print(lista)
+mis_sedes = obtener_sedes()     # 2. "Atrapamos" la lista completa en una variable
+print(mis_sedes)                # 3. Ahora podemos usar esa variable para lo que queramos
 
+# 4. Ejemplo de REUTILIZACIÓN:
+# Como es una lista, podemos saber cuántas sedes hay
+print(f"En total tenemos {len(mis_sedes)} sedes.")
